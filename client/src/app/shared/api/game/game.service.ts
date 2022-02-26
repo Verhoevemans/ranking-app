a22b85c6a69766e04825525a389f40146423b397
verho <verhoevemans@gmail.com>
2022-02-26 11:51:42 +0100

    Updating Angular version

--- client/src/app/shared/api/game/game.service.ts
+++ client/src/app/shared/api/game/game.service.ts
@@ -7,30 +7,30 @@ import { Game } from '../../models/game.model';
 
 @Injectable()
 export class GameService {
-  
+
   private gameId: number;
-  
+
   constructor(private httpClient: HttpClient) {}
-  
+
   createNewGame(game): Observable<Game> {
     return this.httpClient.post<Game>(
       'api/game/create',
       game
     ).pipe(
-      tap((game) => {
-        this.gameId = game.id;
+      tap((newGame) => {
+        this.gameId = newGame.id;
       })
     );
   }
-  
+
   getGame(id: number): Observable<Game> {
     return this.httpClient.get<Game>(`api/game/get/${ id }`);
   }
-  
+
   getGameId(): number {
     return this.gameId;
   }
-  
+
   setGameId(id: number): void {
     console.log('setGameId()', id);
     this.gameId = id;
